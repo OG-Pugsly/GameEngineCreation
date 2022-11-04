a7abb6fce16edfad5e8eca53dbf19f014f2a9163
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2022-11-04 10:54:16 +0000

    Current Card Matching Game

---
+++ Program 24 - Matching Pairs/Program 24 - Matching Pairs/Source.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+char cards[10] = 
+{
+	'A' , 'A' , 'B' , 'B' , 'C' , 'C' , 'D' , 'D' , 'E' , 'E'
+};
+bool cardsFound[5] =
+{
+	false, false, false, false, false
+};
+int numOfTries = 0;
+
+void InitialiseDeck();
+string DisplayCards();
+bool TakeGuess(int cardOne, int cardTwo);
+
+int main()
+{
+	InitialiseDeck();
+	cout << DisplayCards();
+}
+
+
+
+
+
+void InitialiseDeck()
+{
+	shuffle(cards, cards + 10, default_random_engine(69));
+}
+
+string DisplayCards()
+{
+	string CardDisplay;
+	for (int card = 0; card < 10; card++)
+	{
+		if (card == 5)
+		{
+			CardDisplay += "\n";
+		}
+		CardDisplay += "[";
+		CardDisplay += cards[card];
+		CardDisplay += "]";
+	}
+
+	return CardDisplay;
+}
+
+bool TakeGuess(int cardOne, int cardTwo)
+{
+	return false;
+}

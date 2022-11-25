b7c8eb491884d3aa6c6f4c77c4a2571df2effa3a
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2022-11-25 10:12:54 +0000

    Commit 2
    Cleaned up Replacing Characters program

--- Program 26 - Replacing Characters/Program 26 - Replacing Characters/Source.cpp
+++ Program 26 - Replacing Characters/Program 26 - Replacing Characters/Source.cpp
@@ -7,14 +7,16 @@ int main()
 {
 	string userInput;
 	string replacementChar = "X";
-	cout << "Please enter a sentance of text" << endl;
+	cout << "Please enter a sentence of text" << endl;
 	getline(cin, userInput);
 
+	cout << userInput << endl;
 	for (int i = 0; i < size(userInput); i++)
 	{
 		if (userInput[i] == 'e')
 		{
-			userInput.replace(userInput[i], userInput[i], replacementChar);
+			userInput.replace(i, 1, replacementChar);
 		}
 	}
+	cout << userInput << endl;
 }

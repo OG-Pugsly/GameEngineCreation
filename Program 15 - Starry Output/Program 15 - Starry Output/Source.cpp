86220f91ae7cec888730973fb9396edfa482d244
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2022-12-08 23:26:06 +0000

    Program 15
    Added pausing functionality to allow user to read Goodbye statement on program close.

--- Program 15 - Starry Output/Program 15 - Starry Output/Source.cpp
+++ Program 15 - Starry Output/Program 15 - Starry Output/Source.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-
 	int numChoice;
 	char playAgainChoice = ' ';
 	bool playAgain = true;
 
 	while (playAgain)
 	{
-
 		cout << "Please choose a number between 1 and 10" << endl;
 		cin >> numChoice;
 		for (int row = 0; row <= numChoice; row++)
@@ -21,10 +20,8 @@ int main()
 			}
 			cout << endl;
 		}
-
 		cout << "Would you like to play again? Enter Y or N (not case sensitive)." << endl;
 		cin >> playAgainChoice;
-
 		if (playAgainChoice == 'y' || playAgainChoice == 'Y')
 		{
 			playAgain = true;
@@ -34,8 +31,8 @@ int main()
 			playAgain = false;
 
 			cout << "Goodbye" << endl;
-			
+			Sleep(3000);
+			return 0;
 		}
 	}
-
 }

fc4c784cb581c049cf814cb48de9fe955b745fc9
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2022-12-16 12:27:43 +0000

    Program 38 - Using References
    Creation of Program 38.
    This program displays use of references to modify a variable value.

---
+++ Program 38 - Using References/Program 38 - Using References/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void Add25(int& RefNum);
+void Subtract25(int& RefNum);
+
+int main()
+{
+	int num;
+	int& rNum = num;
+	cout << "Please enter a number" << endl;
+	cin >> num;
+	cout << num << endl;   
+	Add25(rNum);
+	cout << num << endl;
+	Subtract25(rNum);
+	cout << num << endl;
+}
+
+void Add25(int& RefNum)
+{
+	RefNum += 25;
+}
+
+void Subtract25(int& RefNum)
+{
+	RefNum -= 25;
+}
